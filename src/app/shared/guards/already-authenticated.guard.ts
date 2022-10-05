f4c32449f627c845c90ea9753cc3fa6d33de35c4
Andriy-FrfR <andriy.lukashewwich@gmail.com>
2022-10-05 15:05:23 +0300

    fixed work of guards so now when restarting page on guarded page user is not getting redirected to home page

--- src/app/shared/guards/already-authenticated.guard.ts
+++ src/app/shared/guards/already-authenticated.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { catchError, Observable, Subscriber, switchMap } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
 
@@ -7,12 +8,19 @@ import { AuthService } from '../services/auth.service';
 export class AlreadyAuthenticatedGuard implements CanActivate {
   constructor(private authServ: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (!this.authServ.isAuthenticated()) {
-      return true;
-    } else {
-      this.router.navigate(['/']);
-      return false;
-    }
+  canActivate(): Observable<boolean> {
+    return this.authServ.getCurrentUser().pipe(
+      switchMap(() => {
+        return new Observable((subscriber: Subscriber<boolean>) => {
+          this.router.navigate(['/']);
+          subscriber.next(false);
+        });
+      }),
+      catchError(() => {
+        return new Observable((subscriber: Subscriber<boolean>) => {
+          subscriber.next(true);
+        });
+      })
+    );
   }
 }
--- src/app/shared/guards/not-authenticated.guard.ts
+++ src/app/shared/guards/not-authenticated.guard.ts
@@ -1,17 +1,25 @@
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { switchMap, Observable, Subscriber, catchError } from 'rxjs';
 
 @Injectable()
 export class NotAuthenticatedGuard implements CanActivate {
   constructor(private authServ: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.authServ.isAuthenticated()) {
-      return true;
-    } else {
-      this.router.navigate(['/']);
-      return false;
-    }
+  canActivate(): Observable<boolean> {
+    return this.authServ.getCurrentUser().pipe(
+      switchMap(() => {
+        return new Observable((subscriber: Subscriber<boolean>) => {
+          subscriber.next(true);
+        });
+      }),
+      catchError(() => {
+        return new Observable((subscriber: Subscriber<boolean>) => {
+          this.router.navigate(['/']);
+          subscriber.next(false);
+        });
+      })
+    );
   }
 }
